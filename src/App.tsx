60e61c94b711e0741ffa546cdb604b6f57797822
Kevin Mbitkebeyo <mbitkebeyokevin@gmail.com>
2023-07-15 06:57:51 +0200

    initialize project implement app file

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import styles from "./style";
+
+function App() {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
